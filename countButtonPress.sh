e19e213a0014e9a163acf1600281641beb7f8d85
princebarpaga <prince95@my.yorku.ca>
2018-11-27 14:24:53 +0000

    final changes

--- countButtonPress.sh
+++ countButtonPress.sh
@@ -3,20 +3,12 @@
 # 0 RED, 1 BLUE, 2 YELLOW, 3 GREEN, 4 BUZZER, 5 BUTTON
 
 countsbutton=0
-counter=0
-read=$(gpio read 5)
 
 while true
 do
-    read=$(gpio read 5)
-    if [ "$read" -eq 0 ]
-    then
-        
-        ((countsbutton++))
 	./waitForButtonPress.sh
-	((counter++))
-	echo "$counter"
         ./setbits.sh "$countsbutton"
+        ((countsbutton++))
 
         if [ "$countsbutton" -eq 16 ]
         then
@@ -29,5 +21,5 @@ do
             sleep 1
             gpio write 4 0 # buzzer stops beeping
         fi
-    fi
+    
 done
